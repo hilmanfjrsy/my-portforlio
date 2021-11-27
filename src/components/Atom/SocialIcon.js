cf0b114889e3b58094288196d0e2377141b9ff49
Hilman Fajri Fahriansyah <hilmanfajri24@gmail.com>
2021-11-27 22:18:51 +0700

    update

--- src/components/Atom/SocialIcon.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class SocialIcon extends Component{
-    render(){
-        return <a href={this.props.href} target="_blank"><i className={this.props.iconName}></i></a>
-    }
-}
